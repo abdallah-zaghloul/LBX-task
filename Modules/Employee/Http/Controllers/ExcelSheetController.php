f47fd0b1d42fbbf557c8bf1398b92e9c2f1ebff5
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2023-10-03 18:42:44 +0300

    Swagger Open API Documentaion Using L5-Swagger

--- Modules/Employee/Http/Controllers/ExcelSheetController.php
+++ Modules/Employee/Http/Controllers/ExcelSheetController.php
@@ -11,9 +11,66 @@ use Throwable;
 class ExcelSheetController extends Controller
 {
     use Response;
-
-
     /**
+     * @OA\Get(
+     *      path="/api/excelSheet/{id}",
+     *      operationId="showExcelSheetById",
+     *      tags={"ExcelSheet"},
+     *      summary="Show ExcelSheet",
+     *      description="Return ExcelSheet",
+     *      operationId="showExcelSheet",
+     *     @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          description="excelSheetID as UUID like 9a4827a1-37ca-4222-b91b-3b551ba74d28",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="string",
+     *           )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *               @OA\Property(property="status", type="bool"),
+     *               @OA\Property(property="message", type="string", default="Successfull operation."),
+     *               @OA\Property(
+     *                              property="data",
+     *                              type="object",
+     *                              @OA\Property(
+     *                                              property="excel_sheet",
+     *                                              type="object",
+     *                                              default={
+                                                                "id": "9a48014d-fcc9-44ac-a13d-c9e72645f284",
+                                                                "path": "ExcelSheet/9a48014d-fcc9-44ac-a13d-c9e72645f284.csv",
+                                                                "status": "Processing",
+                                                                "errors": null,
+                                                                "created_at": "03/10/2023 01:41:43 PM",
+                                                                "updated_at": "03/10/2023 01:41:43 PM",
+                                                                "url": "https://large-file-upload-laravel.s3.eu-central-1.amazonaws.com/ExcelSheet/9a48014d-fcc9-44ac-a13d-c9e72645f284.csv",
+                                                                "updates_url": "http://localhost:8000/api/excelSheet/9a48014d-fcc9-44ac-a13d-c9e72645f284"
+                                                    })
+     *                        ),
+     *               ),
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not Found",
+     *          @OA\JsonContent(
+     *               @OA\Property(property="status", type="bool", default=false),
+     *               @OA\Property(property="message", type="string", default="Not found."),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=503,
+     *          description="Unavailable Server",
+     *          @OA\JsonContent(
+     *               @OA\Property(property="status", type="bool", default=false),
+     *               @OA\Property(property="message", type="string", default="Sorry something went wrong ... please try again later."),
+     *          )
+     *      ),
+     * )
+     *
      * @param string|int $id
      * @param ShowExcelSheetService $service
      * @return JsonResponse
